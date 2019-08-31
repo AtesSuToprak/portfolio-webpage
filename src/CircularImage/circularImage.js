b4ae189b79df277723979ec76fca58d3d1c8d77c
Hakan <hakanyildiz248@gmail.com>
2019-08-31 15:30:48 +0300

    Some changes on the CSS for the animation

--- src/CircularImage/circularImage.js
+++ src/CircularImage/circularImage.js
@@ -4,14 +4,13 @@ import styles from "./styles.module.css";
 class CircularImage extends React.Component {
   render() {
     return (
-      <div className={styles.circleImage}>
+      <figure className={styles.circleImage}>
         <img
           className={styles.image_bg}
           src={require("./image/hakan.jpg")}
           alt="Hakan Yildiz"
         />
-        <div className={styles.innerDiv}></div>
-      </div>
+      </figure>
     );
   }
 }
