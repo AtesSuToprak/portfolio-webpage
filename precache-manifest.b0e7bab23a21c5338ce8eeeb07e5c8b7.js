9d3a34b76e68d9f11d5fb14b35d741ad20b37370
Hakan <hakanyildiz248@gmail.com>
2019-09-22 18:59:07 +0300

    Updates

---
+++ precache-manifest.b0e7bab23a21c5338ce8eeeb07e5c8b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ca347cad2bdee952570a33517cb82d",
+    "url": "/portfolio-webpage/index.html"
+  },
+  {
+    "revision": "9f28d23fff9737559fe3",
+    "url": "/portfolio-webpage/static/css/main.9aaa0e56.chunk.css"
+  },
+  {
+    "revision": "97a225ff368154f232ad",
+    "url": "/portfolio-webpage/static/js/2.9a5e0ff4.chunk.js"
+  },
+  {
+    "revision": "9f28d23fff9737559fe3",
+    "url": "/portfolio-webpage/static/js/main.f862a97e.chunk.js"
+  },
+  {
+    "revision": "899034b59581a7a0dec9",
+    "url": "/portfolio-webpage/static/js/runtime~main.1722b710.js"
+  },
+  {
+    "revision": "2c9a311d36093140bb2d99dd0130c64e",
+    "url": "/portfolio-webpage/static/media/hakan.2c9a311d.jpg"
+  }
+]);
